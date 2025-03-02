237616d9efa744eab8ad337d135c4f8fd88cee18
nileshkanzariya1000 <nkanzariya582@rku.ac.in>
2025-03-02 12:24:02 +0530

    change success ,msg in userLogin.js

--- Api_request/userLogin.js
+++ Api_request/userLogin.js
@@ -3,29 +3,30 @@ async function userLogin(req, res) {
   const { email, password, ...extraFields } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: 'Missing email or password' });
+    return res.status(400).json({ success:false,message: 'Missing email or password' });
   }
 
   if (Object.keys(extraFields).length > 0) {
-    return res.status(400).json({ error: 'Unexpected fields in request' });
+    return res.status(400).json({ success:false,message: 'Unexpected fields in request' });
   }
 
   try {
     const result = await client.query('SELECT * FROM users WHERE email = $1', [email]);
 
     if (result.rows.length === 0) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+      return res.status(401).json({ success:false,message: 'Invalid email or password' });
     }
 
     const userData = result.rows[0];
     if (userData.password !== password) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+      return res.status(401).json({ success:false,message: 'Invalid email or password' });
     }
     if(userData.status !== 0){
-        return res.status(403).json({ error: 'Account is inactive' });
+        return res.status(403).json({ success:false,message: 'Account is inactive' });
     }
 
-    res.json({ message: 'Login successful',
+    res.json({ success:true,
+      message: 'Login successful',
         user_id:userData.user_id,
         user_name:userData.name,
         user_email:userData.email,
@@ -33,7 +34,7 @@ async function userLogin(req, res) {
         user_rfid:userData.rfid
      });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ success:false,error: err.message });
   }
 }
 
